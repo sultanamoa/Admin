941c0b0b095b8734afdb4b5bf489bf9459c58792
sultanamoa <50047806+sultanamoa@users.noreply.github.com>
2020-08-13 00:31:24 +0300

    Update footer.php

--- scripts/pi-hole/php/footer.php
+++ scripts/pi-hole/php/footer.php
@@ -63,9 +63,10 @@
         <div class="row row-centered text-center">
             <div class="col-xs-12 col-sm-6">
                 <strong><a href="https://twitter.com/zoz_alotaibi" rel="noopener" target="_blank"><i class="fa fa-heart text-red"></i> أنقر هنا</a> لمزيد من المعلومات التقنية </strong> 
+				</div>
+				<div class="row row-centered text-center">
 				<strong> قد تكون هناك اعلانات تخرج من حين لاخر وهذا بسبب ان الاعلان متغير بتغير الروابط المستخدمة في نفس الفديو </strong>
-			</div>
-			
+									
         </div>
 
         <div class="row row-centered text-center version-info">
