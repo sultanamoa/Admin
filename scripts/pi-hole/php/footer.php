0ccc5896bd5fb75adf917d7d2204184cde3ce223
sultanamoa <50047806+sultanamoa@users.noreply.github.com>
2020-08-12 22:48:08 +0300

    Update footer.php

--- scripts/pi-hole/php/footer.php
+++ scripts/pi-hole/php/footer.php
@@ -44,20 +44,20 @@
     // Run update checker
     //  - determines local branch each time,
     //  - determines local and remote version every 30 minutes
-#    require "scripts/pi-hole/php/update_checker.php";
+    require "scripts/pi-hole/php/update_checker.php";
 
-     $coreVersionStr = $core_current . (isset($core_commit) ? " (" . $core_branch . ", " . $core_commit . ")" : "");
-     $webVersionStr = $web_current . (isset($web_commit) ? " (" . $web_branch . ", " . $web_commit . ")" : "");
-#    $ftlVersionStr = $FTL_current . (isset($FTL_commit) ? " (" . $FTL_branch . ", " . $FTL_commit . ")" : "");
+    $coreVersionStr = $core_current . (isset($core_commit) ? " (" . $core_branch . ", " . $core_commit . ")" : "");
+    $webVersionStr = $web_current . (isset($web_commit) ? " (" . $web_branch . ", " . $web_commit . ")" : "");
+    $ftlVersionStr = $FTL_current . (isset($FTL_commit) ? " (" . $FTL_branch . ", " . $FTL_commit . ")" : "");
 
-    $githubBaseUrl = "https://github.com/sultanamoa";
-    $coreUrl = $githubBaseUrl . "/Admin";
-#   $webUrl = $githubBaseUrl . "/AdminLTE";
+    $githubBaseUrl = "https://github.com/pi-hole";
+    $coreUrl = $githubBaseUrl . "/pi-hole";
+    $webUrl = $githubBaseUrl . "/AdminLTE";
     $ftlUrl = $githubBaseUrl . "/FTL";
 
     $coreReleasesUrl = $coreUrl . "/releases";
-#   $webReleasesUrl = $webUrl . "/releases";
-#   $ftlReleasesUrl = $ftlUrl . "/releases";
+    $webReleasesUrl = $webUrl . "/releases";
+    $ftlReleasesUrl = $ftlUrl . "/releases";
 ?>
     <footer class="main-footer">
         <div class="row row-centered text-center">
@@ -72,18 +72,25 @@
                 <ul class="list-unstyled">
                     <li><strong>حجب المواقع</strong> <?php echo $coreVersionStr; ?></li>
                     <li><strong>نظام التشغيل</strong> <?php echo $webVersionStr; ?></li>
-                    <li><strong>FTL</strong> <?php echo ; ?></li>
+                    <li><strong>FTL</strong> <?php echo $ftlVersionStr; ?></li>
                 </ul>
                 <?php } else { ?>
                 <ul class="list-inline">
                     <li>
                         <strong>حجب المواقع</strong>
-                 
+                        <a href="<?php echo $coreReleasesUrl . "/" . $core_current; ?>" rel="noopener" target="_blank"><?php echo $core_current; ?></a>
+                        <?php if ($core_update) { ?> &middot; <a class="lookatme" href="<?php echo $coreReleasesUrl . "/latest"; ?>" rel="noopener" target="_blank">Update available!</a><?php } ?>
                     </li>
                     <li>
                         <strong>نظام التشغيل</strong>
+                        <a href="<?php echo $webReleasesUrl . "/" . $web_current; ?>" rel="noopener" target="_blank"><?php echo $web_current; ?></a>
+                        <?php if ($web_update) { ?> &middot; <a class="lookatme" href="<?php echo $webReleasesUrl . "/latest"; ?>" rel="noopener" target="_blank">Update available!</a><?php } ?>
                     </li>
-
+                    <li>
+                        <strong>FTL</strong>
+                        <a href="<?php echo $ftlReleasesUrl . "/" . $FTL_current; ?>" rel="noopener" target="_blank"><?php echo $FTL_current; ?></a>
+                        <?php if ($FTL_update) { ?> &middot; <a class="lookatme" href="<?php echo $ftlReleasesUrl . "/latest"; ?>" rel="noopener" target="_blank">Update available!</a><?php } ?>
+                    </li>
                 </ul>
                 <?php } ?>
             </div>
