789e3480a292076b24e076c6b344d718d99bef36
sultanamoa <50047806+sultanamoa@users.noreply.github.com>
2020-08-14 16:43:29 +0300

    Update dns_records.php

--- dns_records.php
+++ dns_records.php
@@ -11,8 +11,8 @@
 
 <!-- Title -->
 <div class="page-header">
-    <h1>Local DNS Records</h1>
-    <small>On this page, you can add domain/IP associations</small>
+    <h1> قائمة النطاقات الداخلية </h1>
+    <small> في هذه الصفحة يمكنة اضافة اقترانات المجال مع العناوين </small>
 </div>
 
 <!-- Domain Input -->
@@ -22,24 +22,24 @@
             <!-- /.box-header -->
             <div class="box-header with-border">
                 <h3 class="box-title">
-                    Add a new domain/IP combination
+                    اضافة عنوان جديد / نطاق
                 </h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
                 <div class="row">
                     <div class="form-group col-md-6">
-                        <label for="domain">Domain:</label>
+                        <label for="domain">النطاق:</label>
                         <input id="domain" type="url" class="form-control" placeholder="Add a domain (example.com or sub.example.com)" autocomplete="off" spellcheck="false" autocapitalize="none" autocorrect="off">
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="ip">IP Address:</label>
+                        <label for="ip"> العنوان:</label>
                         <input id="ip" type="text" class="form-control" placeholder="Associated IP address" autocomplete="off" spellcheck="false" autocapitalize="none" autocorrect="off">
                     </div>
                 </div>
             </div>
             <div class="box-footer clearfix">
-                <button type="button" id="btnAdd" class="btn btn-primary pull-right">Add</button>
+                <button type="button" id="btnAdd" class="btn btn-primary pull-right"> إضافة</button>
             </div>
         </div>
     </div>
@@ -48,26 +48,26 @@
 <!-- Alerts -->
 <div id="alInfo" class="alert alert-info alert-dismissible fade in" role="alert" hidden>
     <button type="button" class="close" data-hide="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    Updating the custom DNS entries...
+    تحديث ادخالات النطاقات المخصصة
 </div>
 <div id="alSuccess" class="alert alert-success alert-dismissible fade in" role="alert" hidden>
     <button type="button" class="close" data-hide="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    Success! The list will refresh.
+    تهانينا .. القائمة سوف تتحدث
 </div>
 <div id="alFailure" class="alert alert-danger alert-dismissible fade in" role="alert" hidden>
     <button type="button" class="close" data-hide="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    Failure! Something went wrong, see output below:<br/><br/><pre><span id="err"></span></pre>
+    للأسف .. حصل خطب ما , انظر للتفاصيل بالأسفل:<br/><br/><pre><span id="err"></span></pre>
 </div>
 <div id="alWarning" class="alert alert-warning alert-dismissible fade in" role="alert" hidden>
     <button type="button" class="close" data-hide="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    At least one domain was already present, see output below:<br/><br/><pre><span id="warn"></span></pre>
+    أخيرا تم اضافة نطاق محدد القي نظرة في الاسفل:<br/><br/><pre><span id="warn"></span></pre>
 </div>
 <div class="row">
     <div class="col-md-12">
         <div class="box" id="recent-queries">
             <div class="box-header with-border">
                 <h3 class="box-title">
-                    List of local DNS domains
+                    قائمة بالنطاقات الداخلية للشبكة
                 </h3>
             </div>
             <!-- /.box-header -->
@@ -75,13 +75,13 @@
                 <table id="customDNSTable" class="table table-striped table-bordered" width="100%">
                     <thead>
                     <tr>
-                        <th>Domain</th>
-                        <th>IP</th>
-                        <th>Action</th>
+                        <th>النطاق</th>
+                        <th>العنوان</th>
+                        <th>الأمر</th>
                     </tr>
                     </thead>
                 </table>
-                <button type="button" id="resetButton" class="btn btn-default btn-sm text-red hidden">Clear Filters</button>
+                <button type="button" id="resetButton" class="btn btn-default btn-sm text-red hidden">أزالة الفلاتر</button>
             </div>
             <!-- /.box-body -->
         </div>
